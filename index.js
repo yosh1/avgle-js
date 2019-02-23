4b507587ee52b2830e9bae94dd9cf4e4ceeaa6f7
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-02-24 08:39:44 +0900

    [Update] Run方法を更新

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const request = require('request');
-const AVGLE_SEARCH_VIDEOS_API_URL = 'https://api.avgle.com/v1/search/';
-var query = 'AV';
-var page = 2;
-var limit = '?limit=2';
-request(AVGLE_SEARCH_VIDEOS_API_URL + encodeURIComponent(query) + '/' + page + limit, (error, response, body) => {
-    var response = JSON.parse(body);
-    console.log(response);
-    if (response.success) {
-        var videos = response.response.videos;
-        console.log(videos[0].title);
-    }
-});
+const request = require('request'),
+    AVGLE_SEARCH_VIDEOS_API_URL = 'https://api.avgle.com/v1/search/';
+let query = 'AV',
+    page = 2,
+    limit = '?limit=2';
+request(AVGLE_SEARCH_VIDEOS_API_URL + encodeURIComponent(query) + '/' + page + limit,
+    (error, response, body) => {
+        var response = JSON.parse(body);
+        // console.log(response);
+
+        if (response.success) {
+            var videos = response.response.videos;
+            console.log(videos[0].title);
+        }
+
+    });
